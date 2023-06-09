4fd884c6dbb209a73a26fedaeecf7b3ba3fefc44
Peter <peter.lakatos@outlook.de>
2023-06-09 14:50:19 +0200

    Revert "create login and regist component"
    This reverts commit 1fb5fc6f635b07875ced653c5f60958d5d53555f.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+<<<<<<< HEAD
 
 
 const routes: Routes = [
 
 ];
+=======
+
+const routes: Routes = [];
+>>>>>>> parent of 1fb5fc6 (create login and regist component)
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
