1fb5fc6f635b07875ced653c5f60958d5d53555f
Peter <peter.lakatos@outlook.de>
2023-06-09 14:01:54 +0200

    create login and regist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/component/login/login.component';
+import { RegistComponent } from './login/component/regist/regist.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegistComponent
   ],
   imports: [
     BrowserModule,
