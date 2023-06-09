ad37bb72a117951c8a0e4bb6714603dd84fcdb88
Peter <peter.lakatos@outlook.de>
2023-06-09 14:51:22 +0200

    delete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,19 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 
 
 @NgModule({
   declarations: [
     AppComponent,
 
-=======
-
-@NgModule({
-  declarations: [
-    AppComponent
->>>>>>> parent of 1fb5fc6 (create login and regist component)
   ],
   imports: [
     BrowserModule,
