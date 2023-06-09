51f0e7526cf3833620e9b6511ce4a3961281edc5
Peter <peter.lakatos@outlook.de>
2023-06-09 14:49:22 +0200

    delete login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/component/login/login.component';
-import { RegistComponent } from './login/component/regist/regist.component';
+
 
 const routes: Routes = [
-  { path: 'login_module', loadChildren: () => import('./login/login.module').then(login => login.LoginModule) },
-  { path: 'login', component: LoginComponent },
-  { path: '',redirectTo: 'login', pathMatch: 'full' },
-  { path: 'regist', component: RegistComponent }
+
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/component/login/login.component';
-import { RegistComponent } from './login/component/regist/regist.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
-    RegistComponent
+
   ],
   imports: [
     BrowserModule,
