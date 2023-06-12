c0ef95a10e71e59c0cb29eb2f0c99ad4548a2104
Peter <peter.lakatos@outlook.de>
2023-06-12 18:52:51 +0200

    darkmode

--- src/app/dashboard-overview/component/dashboard-overview/dashboard-overview.component.ts
+++ src/app/dashboard-overview/component/dashboard-overview/dashboard-overview.component.ts
@@ -4,41 +4,44 @@ import { withNoDomReuse } from '@angular/platform-browser';
 @Component({
   selector: 'app-dashboard-overview',
   templateUrl: './dashboard-overview.component.html',
-  styleUrls: ['./dashboard-overview.component.scss']
+  styleUrls: ['./dashboard-overview.component.scss'],
 })
 export class DashboardOverviewComponent {
-  constructor(){}
+  constructor() {}
 
+  private expand: boolean = false;
+  public darkMode: boolean = false;
 
-  private expand:boolean = false;
-
-  expandAside(){
+  expandAside() {
     this.expand = !this.expand;
     const linkText = document.querySelectorAll('.link-text');
-    if(this.expand){
+    if (this.expand) {
       const items = Array.from(linkText);
-     for(const itemList of items){
-      itemList.classList.add('link-style');
-     }
-     
-    }else {
+      for (const itemList of items) {
+        itemList.classList.add('link-style');
+      }
+    } else {
       const items = Array.from(linkText);
-     for(const itemList of items){
-      itemList.classList.remove('link-style');
-     }
-      
+      for (const itemList of items) {
+        itemList.classList.remove('link-style');
+      }
     }
-
-
-    // const linkText = document.querySelectorAll('.link-text');
-    // linkText.style.display = 'block'
-    // const anchor = Array.from(linkText);
-    // for(const list of anchor){
-    //  console.log( list.getElementsByClassName('link-text'));
-     
-    // }
-     
-    
   }
 
+  toggleTheme() {
+    this.darkMode = !this.darkMode;
+    const aside = document.querySelector('#aside') as HTMLElement;
+    const linkText = document.querySelectorAll('.links');
+
+    if (this.darkMode) {
+      aside.style.backgroundColor = 'rgb(18, 18, 27)';
+    } else {
+      aside.style.backgroundColor = 'whitesmoke';
+    }
+
+    document.documentElement.setAttribute(
+      'data-theme',
+      this.darkMode ? 'dark' : 'light'
+    );
+  }
 }
