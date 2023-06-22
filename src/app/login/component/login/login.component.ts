fdcd93e04387afd1ea3c0f302dc56bbad8630495
Peter <peter.lakatos@outlook.de>
2023-06-22 17:36:19 +0200

    login

--- src/app/login/component/login/login.component.ts
+++ src/app/login/component/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { LoginService } from 'src/app/service/login.service';
+import { PersonalService } from 'src/app/service/personal/personal.service';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +9,50 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent {
+  constructor(private personalService:PersonalService,
+    private formBuilder:FormBuilder,
+    private loginService:LoginService){}
+
+
+    public form = this.formBuilder.group({
+      email:['',Validators.compose([Validators.minLength(6),Validators.maxLength(35),Validators.required,Validators.email])],
+      password:['',Validators.compose([Validators.required,Validators.minLength(4),Validators.maxLength(10),Validators.required])],
+      rememberMe:['']
+    })
+
+    public alertText:string = '';
+    public loginWasWrong:boolean = false;
+    private allEmployees:any[] = [];
+
+  /**
+   * switch dark and light mode
+   * @param {Event} event
+   */
+  lightMode(event:any){
+    document.documentElement.setAttribute(
+      'data-theme',
+      event.target.checked ? 'dark' : 'light'
+    );    
+  }
+
+  rememberEmployeLogin(event:any){
+    this.form.controls.rememberMe.setValue(event.target.checked);
+    console.log(event.target.checked);
+  }
+
+   login(){
+
+    this.personalService.getPersonal().subscribe(list => {
+      for(let i = 0; i < list.length; i++){
+        if(list[i].email == this.form.value.email && list[i].id == this.form.value.password){
+           this.loginService.postLogin(this.form.value).subscribe()
+          return
+        }
+      } 
+      this.alertText = `Login failed the email or password is incorrect !`;
+      this.loginWasWrong = true;
+      
+    });
+  }
 
 }
--- src/app/service/login.service.ts
+++ src/app/service/login.service.ts
@@ -1,9 +1,17 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class LoginService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private loginUrl: string = 'https://personal-management-1293e-default-rtdb.firebaseio.com/login.json'
+  postLogin(employee:any):Observable<any>{
+    const data = this.http.post(this.loginUrl,employee);
+    return data;
+  }
 }
