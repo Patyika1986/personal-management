0bca60eb3653262abde4e00bfc0e9673dddbee99
Peter <peter.lakatos@outlook.de>
2023-06-22 08:52:16 +0200

    add currentPersonal length badge

--- src/app/dashboard-overview/component/cards/cards.component.ts
+++ src/app/dashboard-overview/component/cards/cards.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { PersonalService } from 'src/app/service/personal/personal.service';
 
 @Component({
   selector: 'app-cards',
   templateUrl: './cards.component.html',
   styleUrls: ['./cards.component.scss']
 })
-export class CardsComponent {
+export class CardsComponent implements OnInit{
 
-  constructor(private router: Router){}
+  constructor(private router: Router, private personalService: PersonalService){}
 
   public currentOrdersBadge:number = 0;
   public employeeLength: number = 0;
   public totalTimeBadge: number = 0;
   public currentPersonalLength: number = 0;
+
+  ngOnInit(): void {
+    this.personalService.getPersonal().subscribe(list => {
+      this.currentPersonalLength = list.length;      
+    });
+  }
 
 
   navigateToPersonal(){
