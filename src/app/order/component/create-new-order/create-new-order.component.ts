8c14da58fa680d58adc3491ff0435de487a2ef70
Peter <peter.lakatos@outlook.de>
2023-06-22 11:14:10 +0200

    form design

--- src/app/order/component/create-new-order/create-new-order.component.ts
+++ src/app/order/component/create-new-order/create-new-order.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-create-new-order',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./create-new-order.component.scss']
 })
 export class CreateNewOrderComponent {
+
+  constructor(private formBuilder:FormBuilder){}
+
+  public form = this.formBuilder.group({
+    
+  })
 
 }
